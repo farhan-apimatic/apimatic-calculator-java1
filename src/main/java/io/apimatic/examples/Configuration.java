81f3d69c64bdfd6f96b6f9e79c29093a1b9472ac
farhan-apimatic <git@apimatic.io>
2019-03-05 05:35:37 +0000

    calculator

---
+++ src/main/java/io/apimatic/examples/Configuration.java
@@ -0,0 +1,28 @@
+/*
+ * APIMATICCalculatorLib
+ *
+ * This file was automatically generated for testing by APIMATIC v2.0 ( https://apimatic.io ).
+ */
+package io.apimatic.examples;
+
+
+public class Configuration {
+    //The base Uri for API calls
+    public static String baseUri = "http://examples.apimatic.io/apps/calculator";
+
+    //sd
+    //TODO: Replace the gfdsfkl with an appropriate value
+    public static String gfdsfkl = "";
+
+    //TODO: Replace the dsfsdf with an appropriate value
+    public static String dsfsdf = "ds";
+
+    //The username to use with basic authentication
+    //TODO: Replace the basicAuthUserName with an appropriate value
+    public static String basicAuthUserName = "TODO: Replace";
+
+    //The password to use with basic authentication
+    //TODO: Replace the basicAuthPassword with an appropriate value
+    public static String basicAuthPassword = "TODO: Replace";
+
+}
